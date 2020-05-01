b34db7909ec16e8aad751441578ef6fc7b90a1ce
r0802117 <r0802117@student.thomasmore.be>
2020-05-01 19:11:22 +0200

    added code to newGameWindow.xaml.cs + backgroundimage newG^q

--- Munchkin_app/Munchkin_app/NewGameWindow.xaml.cs
+++ Munchkin_app/Munchkin_app/NewGameWindow.xaml.cs
@@ -25,6 +25,9 @@ namespace Munchkin_app
         }
 
         List<TextBox> lijstNamenTextboxen = new List<TextBox>(); // lijst is nodig om achteraf gegevens op te vragen 
+        List<CheckBox> lijstCheckboxen = new List<CheckBox>();
+        
+        
         private void txt_aantalSpelers_KeyUp(object sender, KeyEventArgs e)
         {
             if (int.TryParse(txt_aantalSpelers.Text, out int aantalSpelers))
@@ -42,18 +45,40 @@ namespace Munchkin_app
                         labelNaam.Height = 50;
                         labelNaam.Content = "naam Speler:";                       
                         // add the label             
-                       stackpanel.Children.Add(labelNaam);
-
+                        stackpanel.Children.Add(labelNaam);                     
+                        
 
                         // create the textbox
                         TextBox textbox = new TextBox();
                         textbox.Name = "speler" + i;
                         textbox.FontSize = 40;
+                        textbox.Background = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FCE4B6"));
+                        textbox.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF3C0900"));
                         textbox.Text = "test";
-                        // add to the list
+                        // add textboxes to  list
                         lijstNamenTextboxen.Add(textbox);
-                        // add the textbox
+                        // add the textbox to grid
                         stackpanel.Children.Add(textbox);
+
+
+                        //create the checkboxes
+                        CheckBox radiobuttonMan = new CheckBox();
+                        CheckBox radiobuttonVrouw = new CheckBox();
+                        radiobuttonMan.Name = "radiobuttonMan";
+                        radiobuttonMan.Content = "man" + i;
+                        radiobuttonMan.FontSize = 30;
+                        radiobuttonMan.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF3C0900"));
+                        radiobuttonVrouw.Content = "vrouw";
+                        radiobuttonVrouw.Name = "vrouw" + i;
+                        radiobuttonVrouw.FontSize = 30;
+                        radiobuttonVrouw.Foreground= new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF3C0900"));
+                        // add checkboxes to list
+                        lijstCheckboxen.Add(radiobuttonMan);
+                        lijstCheckboxen.Add(radiobuttonVrouw);
+                        // add the radiobuttons to grid
+                        stackpanel.Children.Add(radiobuttonMan);
+                        stackpanel.Children.Add(radiobuttonVrouw);
+
                     }
                 }
                 else
@@ -65,6 +90,13 @@ namespace Munchkin_app
             {
                 MessageBox.Show("Gelieve een getal in te voeren.");
             }
+        }
+
+        private void btn_terugMainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
         }
     }
 }
