ae103c31198629505421884dd87e8c024b379ad0
YoranAerts <r0812030@student.thomasmore.be>
2020-05-05 11:40:25 +0200

    Added BasisKlasse

---
+++ Munchkin_app/Munchkin_MODELS/BasisKlasse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Munchkin_MODELS
+{
+    public abstract class BasisKlasse : IDataErrorInfo
+    {
+
+        public abstract string this[string columnName] { get; }
+
+        public bool IsGeldig()
+        {
+            return string.IsNullOrWhiteSpace(Error);
+
+        }
+        public string Error
+        {
+            get
+            {
+                string foutmeldingen = "";
+
+                foreach (var item in this.GetType().GetProperties())
+                {
+                    if (item.CanRead)
+                    {
+                        string fout = this[item.Name];
+                        if (!string.IsNullOrWhiteSpace(fout))
+                        {
+                            foutmeldingen += fout + Environment.NewLine;
+                        }
+                    }
+                }
+                return foutmeldingen;
+            }
+        }
+    }
+}
