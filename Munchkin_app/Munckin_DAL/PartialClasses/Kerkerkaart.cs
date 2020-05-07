7f19f843a65eb9e9d69db33d62b9f7b63b0dc57b
r0802117 <r0802117@student.thomasmore.be>
2020-05-07 14:21:16 +0200

    partial class toegevoegd

---
+++ Munchkin_app/Munckin_DAL/PartialClasses/Kerkerkaart.cs
@@ -0,0 +1,47 @@
+﻿using Munchkin_MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munckin_DAL
+{
+    public partial class Kerkerkaart:BasisKlasse
+    {
+        public override string this[string columnName]
+        {
+            get {
+                    if (columnName=="Id" && Id >=0)
+                    {
+                         return "Id mag niet kleiner of gelijk zijn aan 0";
+                    }
+                if (columnName == "Beschrijving_2" && string.IsNullOrWhiteSpace(Beschrijving_2))
+                {
+                     return "beschrijving is verplicht";
+                }
+                if (columnName == "Portie_Ellende" && string.IsNullOrWhiteSpace(Portie_Ellende))
+                {
+                     return "Portie ellende is een verplicht veld";
+                }
+                if (columnName == "Level" && Level >=0 && Level<10)
+                {
+                     return "Level mag niet kleiner of gelijk zijn aan 0 en mag niet groter zijn dan 10";
+                }
+                if (columnName == "Aantal_schatten" && Aantal_schatten <=0)
+                {
+                     return "aantal schatten mag niet gelijk zijn of kleiner dan 0";
+                }
+                if (columnName == "Ondood")
+                {
+                    // return "";
+                }
+                if (columnName == "Aantal_Levels" && Aantal_Levels<=0)
+                {
+                     return "Aantal levels moet groter zijn dan 0";
+                }
+                return "";
+                 }
+        }
+    }
+}
---
+++ Munchkin_app/Munckin_DAL/PartialClasses/Speler.cs
@@ -0,0 +1,35 @@
+﻿using Munchkin_MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munckin_DAL
+{
+    public partial class Speler:BasisKlasse
+    {
+        public override string this[string columnName]
+        {
+            get {
+                if (columnName=="Id" && Id <=0 )
+                {
+                    return "Id moet groter dan 0 zijn.";
+                }
+                if (columnName == "Naam" && string.IsNullOrWhiteSpace(Naam))
+                {
+                     return "Naam moet ingevuld zijn";
+                }
+                if (columnName == "Level" && Level<=0 && Level>10 )
+                {
+                     return "Level moet groter zijn dan 0 en kleiner dan 10";
+                }
+                if (columnName == "Geslacht" && string.IsNullOrWhiteSpace(Geslacht))
+                {
+                    return "Geslacht is een verplicht veld";
+                }
+                return ""; 
+            }
+        }
+    }
+}
